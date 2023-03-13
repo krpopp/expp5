9b88c7a6e29ca838741ab7f9bb36f4cb13c7f4f1
krpopp <kp1640@nyu.edu>
2023-03-13 12:19:03 -0400

    idk this was a while ago!

--- sketch.js
+++ sketch.js
@@ -16,123 +16,126 @@ let sketch = function (p) {
     var translateAmtX = [];
     var translateAmtY = [];
 
-    var carPhase =[];
+    var carYMove = [];
 
-    var anglePhase = 0;    
+    var carPhase = [];
 
-    p.preload = function(){
-      bg = p.loadImage("assets/bg.png");
-      cars[0] = p.loadImage("assets/car-1.png");
-      cars[1] = p.loadImage("assets/car-2.png");
-      cars[2] = p.loadImage("assets/car-3.png");
-      cars[3] = p.loadImage("assets/car-4.png");
-      cars[4] = p.loadImage("assets/car-5.png");
-      cars[5] = p.loadImage("assets/car-6.png");
-      cars[6] = p.loadImage("assets/car-7.png");
-      cars[7] = p.loadImage("assets/car-8.png");
-      for(var i = 0; i < cars.length; i++){
-          //console.log(i);
-          currentAngle[i] = 36;
-          carPhase[i] = 0;
-          translateAmtX[i] = p.width/2;
-          translateAmtY[i] = p.height/2;
-      }
-      p.setupCars();
+    var anglePhase = 0;
+
+    var beginX = [];
+    var beginY = [];
+    var endX = [];
+    var endY = [];
+    var distX = [];
+    var distY = [];
+    var exponent = 4;
+    var step = [];
+    var pct = [];
+
+    p.preload = function () {
+        bg = p.loadImage("assets/bg.png");
+        cars[0] = p.loadImage("assets/car-1.png");
+        //   cars[1] = p.loadImage("assets/car-2.png");
+        //   cars[2] = p.loadImage("assets/car-3.png");
+        //   cars[3] = p.loadImage("assets/car-4.png");
+        //   cars[4] = p.loadImage("assets/car-5.png");
+        //   cars[5] = p.loadImage("assets/car-6.png");
+        //   cars[6] = p.loadImage("assets/car-7.png");
+        //   cars[7] = p.loadImage("assets/car-8.png");
+        for (var i = 0; i < cars.length; i++) {
+            //console.log(i);
+            currentAngle[i] = 36;
+            carPhase[i] = 0;
+            carYMove[i] = 0.73;
+            translateAmtX[i] = p.width / 2;
+            translateAmtY[i] = p.height / 2;
+        }
+        p.setupCars();
     }
 
     p.setup = function () {
-       p.createCanvas(1600, 1068);
-
-       translateAmtX = 0;
-       translateAmtY = 0;
-
-       p.angleMode(p.DEGREES);
-       //p.imageMode(p.CENTER);
+        p.createCanvas(1600, 1068);
+        p.angleMode(p.DEGREES);
     };
 
     p.draw = function () {
-       p.image(bg, 0, 0);
-       p.drawCars();
-       p.moveAngle();
-       //lastMouseX = 0;
+        p.image(bg, 0, 0);
+        p.moveAngle();
+        p.drawCars();
+        //lastMouseX = 0;
     };
 
     p.keyPressed = function () {
-       
+
     };
 
-    p.mousePressed = function(){
+    p.mousePressed = function () {
         lastMouseX = 0;
     }
 
-    p.mouseDragged = function(){
+    p.mouseDragged = function () {
         //lastMouseX = p.mouseX;
-        var moveAmt;
-        if(lastMouseX > 0 || lastMouseX < 0){
-            moveAmt = lastMouseX - p.mouseX;
-            //console.log(moveAmt);
-            for(var i = 0; i < cars.length; i++){
-                //console.log("was " + carXPos[i]);
-                carXPos[i] =  carXPos[i] - moveAmt;
-                //console.log("is now " + carXPos[i]);
-            }
-        } 
-        lastMouseX = p.mouseX;
-    }
-
-    p.setupCars = function(){
-        for(var i = 0; i < cars.length; i++){
-            //carXPos[i] = startX + (i * 30);
-            carXPos[i] = 1000 + (i * 30);
-            //console.log(carXPos[i]);
-            //carYPos[i] = -115;
-            carYPos[i] = 500 + (i * 30);
-        }
-    }
-
-    p.moveAngle = function(){
-        for(var i = 0; i < carXPos.length; i++){
-            //console.log("oi");
-            if(carXPos[i] < 910 && carPhase[i] == 0){
-                //console.log("hit point");
-                carXPos[i] += 10;
-                carYPos[i] += 345;
-                currentAngle[i] = 15;
-                carPhase[i] = 1;
-            } else if(carXPos[i] < 850 && carPhase[i] == 1){
-                carYPos[i] += 15;
-                currentAngle[i] = 14;
-                carPhase[i] = 2;
-            } else if(carXPos[i] < 800 && carPhase[i] == 2){
-                carYPos[i] += 25;
-                currentAngle[i] = 12;
-                carPhase[i] = 3;
-            } else if(carXPos[i] < 750 && carPhase[i] == 3){
-                carYPos[i] += 25;
-                currentAngle[i] = 10;
-                carPhase[i] = 4;
-            } else if(carXPos[i] < 700 && carPhase[i] == 4){
-                carYPos[i] += 70;
-                currentAngle[i] = 5;
-                carPhase[i] = 5;
-            } else if (carXPos[i] < 650 && carPhase[i] == 5){
-                carYPos[i] += 70;
-                currentAngle[i] = 0;
-                carPhase[i] = 6;
-            }
-        }
-        // if(carXPos[0] < p.width/2){
-        //     translateAmtX = translateAmtX - 10;
-        //     translateAmtY = translateAmtY - 10;
-        //     currentAngle = 30;
+        // var moveAmt;
+        // if(lastMouseX > 0 || lastMouseX < 0){
+        //     moveAmt = lastMouseX - p.mouseX;
+        //     for(var i = 0; i < cars.length; i++){
+        //         carXPos[i] =  carXPos[i] - moveAmt;
+        //         carYPos[i] = carYPos[i] - (moveAmt * carYMove[i]);
+        //     }
+        // } 
+        // lastMouseX = p.mouseX;
+        // for(var i = 0; i < cars.length; i++){
+        //     pct[i] += step[i];
+        //     if(pct[i] < 1.0){
+        //         carXPos[i] = beginX[i] + pct[i] * distX[i];
+        //         carYPos[i] = beginY[i] + p.pow(pct[i], exponent) * distY[i];
+        //     }
         // }
     }
 
-    p.drawCars = function(){
+    p.setupCars = function () {
+        for (var i = 0; i < cars.length; i++) {
+            carXPos[i] = 1440 + (i * 30);
+            carYPos[i] = 900 + (i * 30);
+            // carXPos[i] = 100;
+            // carYPos[i] = 800;
+            beginX[i] = carXPos[i];
+            beginY[i] = carYPos[i];
+            endX[i] = 100;
+            endY[i] = 890;
+            distX[i] = endX[i] - beginX[i];
+            distY[i] = endY[i] - beginY[i];
+            step[i] = 0.01;
+            pct[i] = 0.0;
+        }
+    }
 
-        for(var i = 0; i < cars.length; i++){
-            //p.translate(carXPos[i], carYPos[i]);
-            //p.resetMatrix();
+    p.moveAngle = function () {
+        // for(var i = 0; i < cars.length; i++){
+        //     if(carXPos[i] < 800 && carXPos[i] >= 790){
+        //         carYMove[i] = 0.6;
+        //     } else if(carXPos[i] < 790 && carXPos[i] >= 780){
+        //         carYMove[i] = 0.5;
+        //     } else if(carXPos[i] < 780 && carXPos[i] >= 770){
+        //         carYMove[i] = 0.4;
+        //     } else if(carXPos[i] < 770 && carXPos[i] >= 760){
+        //         carYMove[i] = 0.3;
+        //     } else if(carXPos[i] < 760 && carXPos[i] >= 750){
+        //         carYMove[i] = 0.2;
+        //     } else if(carXPos[i] < 750 && carXPos[i] >= 740){
+        //         carYMove[i] = 0.1;
+        //     } else if(carXPos[i] < 740 && carXPos[i] >= 730){
+        //         carYMove[i] = -0.1;
+        //     } else if(carXPos[i] < 730 && carXPos[i] >= 720){
+        //         carYMove[i] = -0.2;
+        //     } else if(carXPos[i] < 500){
+        //         carYMove[i] = -1;
+        //     }
+        // }
+    }
+
+    p.drawCars = function () {
+        for (var i = 0; i < cars.length; i++) {
             p.push();
             p.translate(carXPos[i], carYPos[i]);
             p.rotate(currentAngle[i]);
